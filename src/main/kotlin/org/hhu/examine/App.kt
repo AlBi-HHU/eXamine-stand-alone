e2ffce6afdec36d7307a0f3d1a85e71d67ef15b3
Kasper Dinkla <kasper.dinkla@protonmail.com>
2021-10-04 20:01:48 +0200

    Add help argument.

--- src/main/kotlin/org/hhu/examine/App.kt
+++ src/main/kotlin/org/hhu/examine/App.kt
@@ -6,7 +6,14 @@ import tornadofx.App
 
 /** Launch entry point. */
 fun main(args: Array<String>) {
-    Application.launch(ExamineApp::class.java, *args)
+    // Help argument.
+    if (args.contains("--help") or args.contains("-h")) {
+        println("eXamine is an interactive visualization application that requires a JavaFX GUI to run.")
+        println("Place your data sets in a folder 'data-sets' of your current working directory and run eXamine without further arguments.")
+        println("Data set file formats are described at https://github.com/AlBi-HHU/eXamine-stand-alone")
+    } else {
+        Application.launch(ExamineApp::class.java, *args)
+    }
 }
 
 /** eXamine application. */
